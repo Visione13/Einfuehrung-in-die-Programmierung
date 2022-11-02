edb1e2d191cb88baf59514bda84d4abb90b9e5d3
Visione <marcel-gruenheid@gmx.de>
2022-11-02 22:36:54 +0100

    Complete

--- Java/VL04 Aufgabe 5/src/Dosenberechnung.java
+++ Java/VL04 Aufgabe 5/src/Dosenberechnung.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class Dosenberechnung {
 
 	public static void main(String[] args) {
+		Dosenberechnung Dosenberechnung = new Dosenberechnung();
 		Scanner in = new Scanner(System.in);
-		System.out.println("Bitte den Radius eingeben:");
+		System.out.print("Bitte den Radius eingeben: ");
 		int radius = in.nextInt();
-		System.out.println("Bitte die Höhe eingeben:");
+		System.out.print("Bitte die Höhe eingeben: ");
 		int höhe = in.nextInt();
-		
-		
+		double umfang = Dosenberechnung.umfangBerechnen(radius);
+		double deckelfläche = Dosenberechnung.deckelflächeBerechnen(radius);
+		double mantelfläche = Dosenberechnung.mantelflächeBerechnen(umfang, höhe);
+		double oberfläche = Dosenberechnung.oberflächeBerechnen(deckelfläche, mantelfläche);
+		double volumen = Dosenberechnung.volumenBerechnen(deckelfläche, höhe);
+		System.out.println("Umfang: " + umfang);
+		System.out.println("Deckelfläche: " + deckelfläche);
+		System.out.println("Mantelfläche: " + mantelfläche);
+		System.out.println("Oberfläche: " + oberfläche);
+		System.out.println("Volumen: " + volumen);
 	}
 	
 	public double umfangBerechnen(int radius) {
@@ -21,12 +30,12 @@ public class Dosenberechnung {
 		double deckelfläche = Math.PI * (radius * radius);
 		return deckelfläche;
 	}
-	public double mantelflächeBerechnen(int umfang, int höhe) {
-		int mantelfläche = umfang * höhe;
+	public double mantelflächeBerechnen(double umfang, int höhe) {
+		double mantelfläche = umfang * höhe;
 		return mantelfläche;
 		
 	}
-	public double oberflächeBerechnen(double deckelfläche, int mantelfläche) {
+	public double oberflächeBerechnen(double deckelfläche, double mantelfläche) {
 		double oberfläche = 2*(deckelfläche+mantelfläche);
 		return oberfläche;
 	}
